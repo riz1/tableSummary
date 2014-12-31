56530729cb81dfbebd8125c03aea2167f8f82a28
M. Rizwan <murizwan11@gmail.com>
2014-12-31 14:26:26 +0500

    initial commit

---
+++ jquery.tableSummary.js
@@ -0,0 +1,28 @@
+/*
+example use:
+
+$('#tableID').tableSummary(1,3,4); // pass col number
+
+*/
+(function($){
+  $.fn.tableSummary = function(){ 
+  	
+  		$(this).append('<tfoot> '+ $(this).find('tbody tr:last').html() +' </tfoot>');
+		$(this).find('tfoot tr td').html('');
+		$(this).find('tfoot tr td:first').html('Summary');
+
+  		for(var i = 0; i < arguments.length; i++)
+		{
+			 var total = 0;
+			  $(this).find('tbody tr')
+				  .children("td:nth-child(" + arguments[i] + ")")
+				  .each(function(idx,td) {
+				    total += parseFloat($(td).text());
+			  });
+
+		    $(this).find('tfoot tr td:nth-child('+arguments[i]+')').html(total);
+		}
+  };
+
+})(jQuery);
+
